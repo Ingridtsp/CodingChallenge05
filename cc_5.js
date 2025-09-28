3984665b009d716912d11410ba7a0419100c68e3
Ingridtsp <ingridnicole@usf.edu>
2025-09-28 10:42:46 -0400

    fixed step 6

--- cc_5.js
+++ cc_5.js
@@ -22,18 +22,21 @@ function calculateTaxes(grossPay)
 {return grossPay * 0.15;}
 
 //Step 6: Process Payroll for one employee
-function processPayroll (employee)
-{ const basePay = calculateBasePay(employee.hourlyRate, employee.hoursWorked);
+function processPayroll (employee) {
+  const basePay = calculateBasePay(employee.hourlyRate, employee.hoursWorked);
   const overtimePay = calculateOvertimePay(employee.hourlyRate, employee.hoursWorked);
   const grossPay = basePay + overtimePay;
   const taxes = calculateTaxes(grossPay);
-  const netPay = grossPay - taxes;}
+  const netPay = grossPay - taxes;
 
-return { name: employee.name,
+return { 
+    name: employee.name,
     basePay: basePay.toFixed(2),
     overtimePay: overtimePay.toFixed(2),
     grossPay: grossPay.toFixed(2),
-    netPay: netPay.toFixed(2)};
+    netPay: netPay.toFixed(2)
+};
+}
 
 //Step 7: Loop & Log Payroll report 
 employees.forEach(emp =>
