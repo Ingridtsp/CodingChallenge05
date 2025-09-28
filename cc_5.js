c5b732fb7b7713c5311860c7fe398a3454cad841
Ingridtsp <ingridnicole@usf.edu>
2025-09-28 09:50:12 -0400

    completed step 2: employees array

---
+++ cc_5.js
@@ -0,0 +1,8 @@
+//Step 2: Employee Array
+const empployees =[
+    {name:"Jonathan", hourlyRate: 20, hoursWorked: 38},
+    {name:"James", hourlyRate: 22, hoursWorked: 40}
+    {name:"Jade", hourlyRate: 24, hoursWorked: 36}
+    {name:"Jacob", hourlyRate: 26, hoursWorked: 42}
+    {name:"Jessica", hourlyRate: 21.50, hoursWorked: 45}
+]
