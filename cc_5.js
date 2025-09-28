a60fc58d68d836ef41c1141a51f09f6d24b24aac
Ingridtsp <ingridnicole@usf.edu>
2025-09-28 10:00:50 -0400

    committing step 3 calculate base pay hrs

--- cc_5.js
+++ cc_5.js
@@ -1,8 +1,15 @@
 //Step 2: Employee Array
 const empployees =[
     {name:"Jonathan", hourlyRate: 20, hoursWorked: 38},
-    {name:"James", hourlyRate: 22, hoursWorked: 40}
-    {name:"Jade", hourlyRate: 24, hoursWorked: 36}
-    {name:"Jacob", hourlyRate: 26, hoursWorked: 42}
+    {name:"James", hourlyRate: 22, hoursWorked: 40},
+    {name:"Jade", hourlyRate: 24, hoursWorked: 36},
+    {name:"Jacob", hourlyRate: 26, hoursWorked: 42},
     {name:"Jessica", hourlyRate: 21.50, hoursWorked: 45}
-]
+];
+
+//Step 3: Caclculate base pay (up to 40 hrs)
+function calculateBasePay(rate, hours)
+{const regularHours = Math.min(hours, 40);
+    return rate * regularHours;
+}
+
