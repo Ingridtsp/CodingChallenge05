6f1a9280bc664243ac8f2054e0911bf24dc39f8f
Ingridtsp <ingridnicole@usf.edu>
2025-09-28 10:32:11 -0400

    completing step 7

--- cc_5.js
+++ cc_5.js
@@ -10,20 +10,16 @@ const employees =[
 //Step 3: Caclculate base pay (up to 40 hrs)
 function calculateBasePay(rate, hours)
 {const regularHours = Math.min(hours, 40);
-    return rate * regularHours;
-}
+    return rate * regularHours;}
 
 //Step 4: Calculate overtime pay (time and a half >40 hrs)
 function calculateOvertimePay(rate, hours)
 {const overtimeHours = Math.max(0, hours - 40);
-    return overtimeHours * (rate * 1.5);
-}
+    return overtimeHours * (rate * 1.5);}
 
 //Step 5: Calculate Taxes (15% deduction)
 function calculateTaxes(grossPay)
-{return grossPay * 0.15;
-
-}
+{return grossPay * 0.15;}
 
 //Step 6: Process Payroll for one employee
 function processPayroll (employee)
@@ -31,14 +27,17 @@ function processPayroll (employee)
   const overtimePay = calculateOvertimePay(employee.hourlyRate, employee.hoursWorked);
   const grossPay = basePay + overtimePay;
   const taxes = calculateTaxes(grossPay);
-  const netPay = grossPay - taxes;
-}
+  const netPay = grossPay - taxes;}
 
 return {
     name: employee.name,
     basePay: basePay.toFixed(2),
     overtimePay: overtimePay.toFixed(2),
     grossPay: grossPay.toFixed(2),
-    netPay: netPay.toFixed(2)
+    netPay: netPay.toFixed(2)};
 
-};
+//Step 7: Loop & Log Payroll report 
+employees.forEach(emp =>
+     {console.log(processPayroll(emp));
+
+});
